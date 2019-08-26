a78b26269218ff40c7f2135065cc7e6a5b5e72d1
pratibha499 <pratibha.sharma_cs18@gla.ac.in>
2019-08-25 23:44:13 -0700

    print

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,18 @@
 package Main;
-();
+
+
+import java.util.Scanner;
 
 /**
  * @pratibha version 1.0
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        String vap = userInput.nextLine().trim();
+        System.out.println("Welcome " + vap + "!");
+
+
 
     }
 
