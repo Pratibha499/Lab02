bd5e69489a1dcda418a0b5b78f390e6a5555460f
pratibha499 <pratibha.sharma_cs18@gla.ac.in>
2019-08-29 02:50:36 -0700

    enter temperature in float

--- src/Main/Itemprice.java
+++ src/Main/Itemprice.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Itemprice {
     public static void main(String[] args) {
         Scanner rp = new Scanner(System.in);
---
+++ src/Main/Temperature.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        System.out.println("enter temperature in centigrade");
+        double t = c.nextDouble();
+
+
+    }
+}
