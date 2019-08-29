2dfef8eefb340d59d9ac07932a3f795165efed3d
pratibha499 <pratibha.sharma_cs18@gla.ac.in>
2019-08-29 02:38:33 -0700

    enter float value

---
+++ src/Main/Itemprice.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Itemprice {
+    public static void main(String[] args) {
+        Scanner rp = new Scanner(System.in);
+        System.out.println("enter float value");
+        double n = rp.nextDouble();
+
+
+    }
+
+}
