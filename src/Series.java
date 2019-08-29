74d851973125d9a3d44741cb3f5f74f176e62f8a
pratibha499 <pratibha.sharma_cs18@gla.ac.in>
2019-08-29 02:22:56 -0700

    main method

--- src/Series.java
+++ src/Series.java
@@ -4,6 +4,13 @@ public class Series {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("enter the value");
+        int n = s.nextInt();
+        double sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + (1.0 / i);
+        }
+        System.out.println("series is " + sum);
+
 
 
     }
