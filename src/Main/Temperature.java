5dfde3bf47e09ed08923e3872f1a4422808a62ba
pratibha499 <pratibha.sharma_cs18@gla.ac.in>
2019-08-29 02:57:57 -0700

    main method

--- src/Main/Temperature.java
+++ src/Main/Temperature.java
@@ -5,6 +5,9 @@ public class Temperature {
         Scanner c = new Scanner(System.in);
         System.out.println("enter temperature in centigrade");
         double t = c.nextDouble();
+        double n = (t - 32) / 1.8;
+        System.out.println("temperature in celcius " + n);
+
 
 
     }
